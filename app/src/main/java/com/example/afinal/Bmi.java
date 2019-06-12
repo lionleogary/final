80975e454e802efeec836d685502320da60742ef
leo <lionleogary@gms.tku.edu.tw>
2019-06-13 01:09:15 +0800

    uu

--- app/src/main/java/com/example/afinal/Bmi.java
+++ app/src/main/java/com/example/afinal/Bmi.java
@@ -4,20 +4,32 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class Bmi extends AppCompatActivity{
     private EditText hei;
+
+    private TextView tt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bmi);
+       show();
+
 
 
     }
-    private void show(){
+     public void show ( ){
+        NumberFormat nf=NumberFormat.getInstance();
         Bundle bundle=getIntent().getExtras();
+        int n=bundle.getInt("bh");
+
         hei=findViewById(R.id.height);
-        hei.setText(bundle.getInt("bh"));
+        tt=findViewById(R.id.textView);
+        hei.setText(nf.format(n));
+
     }
     public void finished(View v){
         super.finish();
--- app/src/main/java/com/example/afinal/MainActivity.java
+++ app/src/main/java/com/example/afinal/MainActivity.java
@@ -5,16 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
     private EditText name;
     private EditText height;
+    private RadioGroup rr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         name=findViewById(R.id.editText);
-        name=findViewById(R.id.editText2);
+        height=findViewById(R.id.editText2);
+        rr=(RadioGroup) findViewById(R.id.RRR);
     }
     public void Bmi(View v){
         Intent intent=new Intent(this,Bmi.class);
@@ -25,6 +29,17 @@ public class MainActivity extends AppCompatActivity {
         bundle.putInt("bh",bh);
         intent.putExtras(bundle);
         startActivity(intent);
+    }
+    public void KKK(View v){
+
+       switch( rr.getCheckedRadioButtonId()){
+           case R.id.radioButton:
+               name.setText("IIIIBBBBB");
+               break;
+           case R.id.radioButton2:
+               name.setText("Igggggggg");
+               break;
+       }
     }
 
 }
